ad9c0db9f8da0180c76b898e2f4572aeecae6b8d
jillianbiasotti <jnbiasotti@quinnipiac.edu>
2019-04-17 21:01:55 -0400

    first draft

---
+++ HearthSearchApp/app/src/main/java/edu/quinnipiac/ser210/hearthsearchapp/MainActivity.java
@@ -0,0 +1,34 @@
+package edu.quinnipiac.ser210.hearthsearchapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void onAboutClick(View view) {
+
+    }
+
+    public void onStartClick(View view) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
