9dfc37afd66756b6e144ea2565153d3f837e5398
jillianbiasotti <jnbiasotti@quinnipiac.edu>
2019-04-17 22:34:57 -0400

    search activity changes

--- HearthSearchApp/app/src/main/java/edu/quinnipiac/ser210/hearthsearchapp/FavoritesActivity.java
+++ HearthSearchApp/app/src/main/java/edu/quinnipiac/ser210/hearthsearchapp/FavoritesActivity.java
@@ -1,5 +1,6 @@
 package edu.quinnipiac.ser210.hearthsearchapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class FavoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        Intent intent = getIntent();
+
     }
 }
