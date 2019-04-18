e0ca19607d35129c0d9fc6b0de4d661c1dbcdaa2
joeruiz <joseph.ruiz@quinnipiac.edu>
2019-04-17 22:41:41 -0400

    json updated

--- HearthSearchApp/app/src/main/java/edu/quinnipiac/ser210/hearthsearchapp/JSONDataHandler.java
+++ HearthSearchApp/app/src/main/java/edu/quinnipiac/ser210/hearthsearchapp/JSONDataHandler.java
@@ -6,31 +6,26 @@ import org.json.JSONObject;
 public class JSONDataHandler {
 
 
-    public String getWeatherData(String JSONString) throws JSONException {
-        String name,country,temp,mintemp,maxtemp,speed,degree,description;
-        JSONObject weatherData = new JSONObject(JSONString);
+    public String getCardData(String JSONString) throws JSONException {
+        String name,cardSet,type,health,text,playerClass,imgGold;
+        JSONObject cardData = new JSONObject(JSONString);
 
-        JSONObject main = weatherData.getJSONArray("list").getJSONObject(0).getJSONObject("main");
-        JSONObject wind = weatherData.getJSONArray("list").getJSONObject(0).getJSONObject("wind");
-        JSONObject weather = weatherData.getJSONArray("list").getJSONObject(0).getJSONArray("weather").getJSONObject(0);
+        name = cardData.getString("name");
+        cardSet = cardData.getString("cardSet");
+        type = cardData.getString("type");
+        health = cardData.getString("health");
+        text = cardData.getString("text");
+        playerClass = cardData.getString("PlayerClass");
+        imgGold = cardData.getString("imgGold");
 
-        name = weatherData.getJSONArray("list").getJSONObject(0).getString("name");
-        country = weatherData.getJSONArray("list").getJSONObject(0).getJSONObject("sys").getString("country");
-        temp = main.getString("temp");
-        mintemp = main.getString("temp_min");
-        maxtemp = main.getString("temp_max");
-        speed = wind.getString("speed");
-        degree = wind.getString("deg");
-        description = weather.getString("description");
 
-        String toset = "" + name + " " + country
-                + "\nTemperature: " + temp + "°F"
-                + "\nMinimum Temperature: " + mintemp + "°F"
-                + "\nMaximum Temperature: " + maxtemp + "°F"
-                + "\nWind: "
-                + "\n   Speed: " + speed
-                + "\n   Degree:" + degree + "°"
-                + "\nDescription: " + description;
+        String toset = imgGold
+                + "\n"+ name
+                + "\n"+ playerClass
+                + "\nCard Set: " + cardSet
+                + "\nCard Type: " + type
+                + "\nHealth: " + health
+                + "\n"+ text;
         return toset;
 
     }
