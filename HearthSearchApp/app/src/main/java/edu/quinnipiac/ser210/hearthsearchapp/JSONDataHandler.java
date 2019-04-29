67fc77a41f9b264bf28efb7317fb3f87c5c3a1bc
joeruiz <joseph.ruiz@quinnipiac.edu>
2019-04-29 18:22:32 -0400

    JsonHandler Updated

--- HearthSearchApp/app/src/main/java/edu/quinnipiac/ser210/hearthsearchapp/JSONDataHandler.java
+++ HearthSearchApp/app/src/main/java/edu/quinnipiac/ser210/hearthsearchapp/JSONDataHandler.java
@@ -1,47 +1,63 @@
 package edu.quinnipiac.ser210.hearthsearchapp;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.util.JsonReader;
+import android.util.Log;
+import android.widget.ImageView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.InputStream;
+
 public class JSONDataHandler {
 
 
     public String getCardData(String JSONString) throws JSONException {
-        String name,cardSet,type,health,text,imgGold,playerClass,attack,cost;
+        String name, cardSet, type, health, text, imgGold, playerClass, attack, cost;
 
         JSONArray cardData = new JSONArray(JSONString);
 
-        JSONObject main = cardData.getJSONObject(0);
+        for (int i = 0; i < cardData.length(); i++) {
+            cardData.getJSONObject(i);
 
-        imgGold = main.getString("imgGold");
-
-
-
-        name = main.getString("name");
-        cardSet = main.getString("cardSet");
-        type = main.getString("type");
-        health = main.getString("health");
-        text = main.getString("text");
-        playerClass = main.getString("playerClass");
-        attack = main.getString("attack");
-        cost = main.getString("cost");
-
+        }
 
         String toset =
-                  "Card Name: "+ name
-                + "\nPlayer Class: "+ playerClass
-                + "\nCard Set: " + cardSet
-                + "\nCard Type: " + type
-                + "\nCost: " + cost
-                + "\nAttack: " + attack
-                + "\nHealth: " + health;
-                //+ "\n"+ text;
+                "Card Name: " + cardData.getJSONObject(0).getString("name") +
+                        "Card Name: " + cardData.getJSONObject(1).getString("name");
+
         return toset;
 
     }
 
+
+    public static class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL = urls[0];
+            Bitmap bimage = null;
+            try {
+                InputStream in = new java.net.URL(imageURL).openStream();
+                bimage = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+    }
 }
